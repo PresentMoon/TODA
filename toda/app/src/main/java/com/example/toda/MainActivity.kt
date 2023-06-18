4e64a234c4b0243c40cfa07bae3f59e94033f8e1
PresentMoon <irishmiguelpulido@gmail.com>
2023-06-18 20:25:55 +0800

    Implementation of Google Maps API

---
+++ toda/app/src/main/java/com/example/toda/MainActivity.kt
@@ -0,0 +1,17 @@
+package com.example.toda
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.google.android.gms.maps.SupportMapFragment
+
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val mapFragment =
+            supportFragmentManager.findFragmentById(R.id.map_fragment) as? SupportMapFragment
+        mapFragment?.getMapAsync { googleMap ->
+        }
+    }
+}
